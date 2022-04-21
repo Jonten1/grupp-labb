ee9c66ef0488ff697a1609ef3e4fad1d35a6972c
Isak Martinson <isak.martinson@iths.se>
2022-04-21 14:37:10 +0200

    jag tror jag löste det

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'rootuser',
-    password : '?',
-    database : 'test'
-})
-
-module.exports = connection
