234561111d8ab3aff7532faae8727adb136a4e10
Jonten1 <jonas.tengborg@gmail.com>
2022-04-21 11:10:44 +0200

    Lade grunden för server.js

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-//Kenneth skriver en kommentar och testar...
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const connection = require('./connection')
+
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+let db
+
+mongo.connect(
+	url,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},
+	(err, client) => {
+		if (err) {
+			console.error(err)
+			return
+		}
+		// Namnet på collection tror jag
+		db = client.db('?')
+		books = db.collection('?')
+	}
+)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors())
+app.use(express.static('public'))
+const port = 3000
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
