b6d43d936968df62bd50b93a7e648f8d5b59eec5
Isak Martinson <isak.martinson@iths.se>
2022-04-21 13:09:25 +0200

    test

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'rootuser',
     password : '?',
-    database : '?'
+    database : 'test'
 })
 
 module.exports = connection
