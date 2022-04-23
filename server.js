c0ad5b2ccd202041afc27acec69b38b9e8961622
kennethwi <23746270+kennethwi@users.noreply.github.com>
2022-04-23 20:18:42 +0200

    Småändringar vid git pull

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const connection = require('./connection')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connection = require("gitignorefolder/connection");
 
-const mongo = require('mongodb').MongoClient
-const url = 'mongodb://localhost:27017'
-let db
+const mongo = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017";
+let db;
 
 mongo.connect(
-	url,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	},
-	(err, client) => {
-		if (err) {
-			console.error(err)
-			return
-		}
-		// Namnet på collection tror jag
-		db = client.db('?')
-		books = db.collection('?')
-	}
-)
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    // Namnet på collection tror jag
+    db = client.db("?");
+    books = db.collection("?");
+  }
+);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors())
-app.use(express.static('public'))
-const port = 3000
+app.use(cors());
+app.use(express.static("public"));
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
