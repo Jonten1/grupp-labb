3a0d3291ed4b35c470cf255758b349369b42b5b1
Jonten1 <jonas.tengborg@gmail.com>
2022-04-25 14:41:02 +0200

    Fixat så att alla filmer som ligger i databasen visas upp som en lista

---
+++ public/js/fetch-filmlista.js
@@ -0,0 +1,25 @@
+function createNode(element) {
+	return document.createElement(element)
+}
+
+function append(parent, el) {
+	return parent.appendChild(el)
+}
+
+const ul = document.getElementById('film')
+const url = 'http://localhost:3000/api/filmer'
+fetch(url)
+	.then((resp) => resp.json())
+	.then(function (data) {
+		console.log(data)
+		console.log('Visa första i json-objektet: ' + data[0].titel)
+		let film = data
+		return film.map(function (data) {
+			let li = createNode('li')
+			li.innerHTML = data.titel
+			append(ul, li)
+		})
+	})
+	.catch(function (error) {
+		console.log(error)
+	})
