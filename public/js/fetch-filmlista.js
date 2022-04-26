0a7f72ae90eac9899bd162f654cfb4b39133662c
Jonten1 <jonas.tengborg@gmail.com>
2022-04-26 15:42:48 +0200

    Provat lite olika metoder för att länka till rätt film

--- public/js/fetch-filmlista.js
+++ public/js/fetch-filmlista.js
@@ -1,25 +1,52 @@
-function createNode(element) {
-	return document.createElement(element)
-}
-
-function append(parent, el) {
-	return parent.appendChild(el)
-}
-
-const ul = document.getElementById('film')
-const url = 'http://localhost:3000/api/filmer'
-fetch(url)
-	.then((resp) => resp.json())
-	.then(function (data) {
-		console.log(data)
-		console.log('Visa första i json-objektet: ' + data[0].titel)
-		let film = data
-		return film.map(function (data) {
-			let li = createNode('li')
-			li.innerHTML = data.titel
-			append(ul, li)
+function fetchData() {
+	fetch('http://localhost:3000/api/filmer')
+		.then((response) => {
+			if (!response.ok) {
+				throw Error('ERROR')
+			}
+			return response.json()
 		})
-	})
-	.catch(function (error) {
-		console.log(error)
-	})
+		.then((data) => {
+			console.log(data)
+			const html = data
+				.map((filmer) => {
+					return `<div class="filmer">
+				<a href="http://localhost:3000/api/filmer/:${filmer.filmId}">${filmer.titel}</a>
+				</div>
+				`
+				})
+				.join('')
+			console.log(html)
+			document.querySelector('#app').insertAdjacentHTML('afterbegin', html)
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
+
+fetchData()
+
+// function createNode(element) {
+// 	return document.createElement(element)
+// }
+
+// function append(parent, el) {
+// 	return parent.appendChild(el)
+// }
+
+// const a = document.getElementById('film')
+// fetch('http://localhost:3000/api/filmer')
+// 	.then((resp) => resp.json())
+// 	.then(function (data) {
+// 		console.log(data)
+// 		console.log('Visa första i json-objektet: ' + data[0].filmId)
+// 		let film = data
+// 		return film.map(function (data) {
+// 			let li = createNode('li')
+// 			li.innerHTML = data.titel
+// 			append(a, li)
+// 		})
+// 	})
+// 	.catch(function (error) {
+// 		console.log(error)
+// 	})
